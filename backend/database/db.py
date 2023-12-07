0646a37dfef108e7dd4c70dc48f7a265c8432700
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-07 12:02:24 +0300

    Created the database file for the application

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,8 +1,38 @@
 """The database definition file for the app"""
-import pymysql
 import os
-from dotenv import load_dotenv, find_dotenv
+
+import pymysql
+from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv(find_dotenv())
+
+
+MYSQL_HOST = os.environ.get("MYSQL_HOST")
+MYSQL_USERNAME = os.environ.get("MYSQL_USERNAME")
+MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
+MYSQL_PORT = os.environ.get("MYSQL_PORT")
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
+
+
+async def get_db():
+    """The function returns a database object instance
+
+    Yields:
+        _db: Database instance
+    """
+    _db = SessionLocal()
+
+    try:
+        yield _db
+    finally:
+        _db.close()

c79f6cae30a3c985054f07543630f43402e8d28b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-07 14:23:25 +0300

    The incidents schema

---
+++ backend/schemas/incident_schema.py
@@ -0,0 +1,68 @@
+"""The schema file for the incidents data"""
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class IncidentBase(BaseModel):
+    """The base schema for the incidents data
+
+    Args:
+        BaseModel (Pydantic): The base class for all schemas
+    """
+    incident_description: str
+    product_name: str
+    product_code: str
+    product_quantity: int
+    product_price: float
+    is_resolved: bool
+
+    store_section_id: int
+    employee_id: int
+    store_id: int
+    region_id: int
+
+
+class CreateIncident(IncidentBase):
+    """The schema used to create incidents in the database
+
+    Args:
+        IncidentBase (Pydantic): The base incident schema
+    """
+    pass
+
+
+class ReadIncident(IncidentBase):
+    """The schema used to read incidents from the database
+
+    Args:
+        IncidentBase (Pydantic): The base incidents schema
+    """
+    incident_id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        """The config class for reading data from the database
+        """
+        from_attributes = True
+
+
+class UpdateIncident(BaseModel):
+    """The schema used to update incidents in the database
+
+    Args:
+        BaseModel (Pydantic): The base class for all schemas
+    """
+    incident_description: Optional[str]
+    product_name: Optional[str]
+    product_code: Optional[str]
+    product_quantity: Optional[int]
+    product_price: Optional[float]
+    is_resolved: Optional[bool]
+
+    class Config:
+        """The config class for reading data from the database
+        """
+        from_attributes = True

4fcbb97a7bd8dca2a63b54feeacd0296588e329b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-07 14:46:52 +0300

    Created the service file for the region data CRUD operations

--- backend/services/region_services.py
+++ backend/services/region_services.py
@@ -1,2 +1,89 @@
 """The services file for the region CRUD operations"""
+from typing import List
+
+from models.models import Regions
 from schemas.region_schema import CreateRegion, ReadRegion, UpdateRegion
+from sqlalchemy.orm import Session
+
+
+def create_region_service(_region_data: CreateRegion, _db: Session) -> ReadRegion:
+    """The service function to add a region to the database
+
+    Args:
+        _region_data (CreateRegion): The data needed to create a new region
+        _db (Session): The database session
+
+    Returns:
+        ReadRegion: The newly created region object
+    """
+    region = Regions(**_region_data.model_dump())
+    _db.add(region)
+    _db.commit()
+    _db.refresh(region)
+    return region
+
+
+def retrieve_all_regions_service(_db: Session) -> List[ReadRegion]:
+    """The service function to retrieve all regions from the database
+
+    Args:
+        _db (Session): The database session
+
+    Returns:
+        List[ReadRegion]: The list of regions
+    """
+    return _db.query(Regions).all()
+
+
+def retrieve_one_region_service(_region_id: int, _db: Session) -> ReadRegion:
+    """The service function to retrieve a region from the database
+
+    Args:
+        _region_id (int): The id of the region
+        _db (Session): The database session
+
+    Returns:
+        ReadRegion: The region object
+    """
+    return _db.query(Regions).filter(Regions.region_id == _region_id).first()
+
+
+def update_region_endpoint(
+    _region_id: int,
+    _update_region_data: UpdateRegion,
+    _db: Session
+) -> ReadRegion:
+    """The service function to update a region in the database
+
+    Args:
+        _region_id (int): The id of the region
+        _update_region_data (UpdateRegion): The data needed to update the region
+        _db (Session): The database session
+
+    Returns:
+        ReadRegion: The updated region object
+    """
+    region = retrieve_one_region_service(_region_id, _db)
+    if not region:
+        return None
+    region.region_name = _update_region_data.region_name
+    _db.commit()
+    _db.refresh(region)
+    return region
+
+
+def delete_region_service(_region_id: int, _db: Session) -> None:
+    """The service function to delete a region from the database
+
+    Args:
+        _region_id (int): The id of the region
+        _db (Session): The database session
+
+    Returns:
+        ReadRegion: The deleted region object
+    """
+    region = retrieve_one_region_service(_region_id, _db)
+    if not region:
+        return None
+    _db.delete(region)
+    _db.commit()

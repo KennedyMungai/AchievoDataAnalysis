02e63e80ddc9e95833423bd080ce474b838f7765
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-09 12:45:55 +0300

    Fixed the page dimentsions

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,8 +2,8 @@ import TopBar from "@/components/TopBar/TopBar";
 
 export default function Home() {
   return (
-    <main className="container min-h-screen bg-slate-200 dark:bg-slate-800">
-      {/* <TopBar pageTitle={"Achievo Limited"} /> */}
+    <main className="w-[94vw] min-h-screen bg-slate-200 dark:bg-slate-800 ml-[6vw]">
+      <TopBar pageTitle={"Achievo Limited"} />
     </main>
   )
 }

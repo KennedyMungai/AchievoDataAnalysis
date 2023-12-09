dd48b6a8362c16601756f33ee363e6b4943f0e81
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-09 11:51:36 +0300

    Created the TopBar component

---
+++ frontend/components/TopBar/TopBar.tsx
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react'
+import LoginButton from './LoginButton'
+
+type Props = {
+	pageTitle: string
+}
+
+const TopBar = ({pageTitle}: Props) => {
+	return (
+		<div className='h-[3rem] bg-slate-50 dark:bg-slate-900 flex items-center justify-between p-3 border-b-[1px] border-slate-300 dark:border-slate-500'>
+			<div className='font-semibold text-2xl uppercase'>{pageTitle}</div>
+			<div className=''>
+				<LoginButton />
+			</div>
+		</div>
+	)
+}
+
+export default TopBar

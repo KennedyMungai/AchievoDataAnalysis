5c50b5cbe30b08f16c4dda18ee9bb698b985e7a2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-09 12:29:38 +0300

    Created the authSlice

--- frontend/redux/features/auth/authSlice.ts
+++ frontend/redux/features/auth/authSlice.ts
@@ -1,3 +1,47 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import axios from 'axios'
+import { RootState } from "../store"
 
+
+export const getAuthToken = createAsyncThunk("auth/login", async (values: ICredentials) => {
+    const response = await axios.post("http://localhost:8000/auth/login", values)
+    const data = response.data
+
+    return data
+})
+
+
+const initialState: IToken = {
+    token_type: "",
+    access_token: "",
+    is_loaded: false
+}
+
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getAuthToken.fulfilled, (state, action) => {
+                state.is_loaded = true
+                state.token_type = action.payload.token_type
+                state.access_token = action.payload.access_token
+            })
+            .addCase(getAuthToken.rejected, (state, action) => {
+                state.is_loaded = false
+                state.token_type = ""
+                state.access_token = ""
+            })
+            .addCase(getAuthToken.pending, (state, action) => {
+                state.is_loaded = false
+                state.token_type = ""
+                state.access_token = ""
+            })
+    }
+})
+
+export const selectAuthStateData = (state: RootState) => state.authState
+
+export default authSlice.reducer

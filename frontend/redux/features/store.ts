5b5870f9cb5be83c4a3f4dbff64087496c92c958
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-09 12:28:40 +0300

    Registered the auth reducer

--- frontend/redux/features/store.ts
+++ frontend/redux/features/store.ts
@@ -1,8 +1,11 @@
-import {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './auth/authSlice'
 
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        authState: authReducer
+    }
 })
 
 

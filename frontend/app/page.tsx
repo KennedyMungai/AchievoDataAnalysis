1587752b4d08efc1a0a7675e66714ce05d144497
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-09 12:06:33 +0300

    Added the bg colors for the app

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <main className="container min-h-screen"></main>
+    <main className="container min-h-screen bg-slate-100 dark:bg-slate-800"></main>
   )
 }

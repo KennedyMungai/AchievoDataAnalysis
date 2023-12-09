34cbbebb0bfb69bbf028378510022b8bd28b96b3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-09 12:46:23 +0300

    Fixed the dimensions of the Topbar component

--- frontend/components/TopBar/TopBar.tsx
+++ frontend/components/TopBar/TopBar.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const TopBar = ({pageTitle}: Props) => {
 	return (
-		<div className='h-[3rem] bg-slate-50 dark:bg-slate-900 flex items-center justify-between p-3 border-b-[1px] border-slate-300 dark:border-slate-500'>
+		<div className='h-[3rem] bg-slate-50 dark:bg-slate-900 flex items-center justify-between p-3 border-b-[1px] border-slate-300 dark:border-slate-500 w-[94vw]'>
 			<div className='font-semibold text-2xl uppercase'>{pageTitle}</div>
 			<div className=''>
 				<LoginButton />

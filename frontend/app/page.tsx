9ffb576bb4c54ea8b46781b79770df30f6df87bc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-09 12:07:55 +0300

    Added a topbar to the page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,9 @@
+import TopBar from "@/components/TopBar/TopBar";
+
 export default function Home() {
   return (
-    <main className="container min-h-screen bg-slate-200 dark:bg-slate-800"></main>
+    <main className="container min-h-screen bg-slate-200 dark:bg-slate-800">
+      <TopBar pageTitle={"Achievo Limited"} />
+    </main>
   )
 }

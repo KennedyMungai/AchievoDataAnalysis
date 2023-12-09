1d1527670d54adc8b0d2e8707925afff6fdd5f2b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-09 12:02:01 +0300

    Created the ThemeProvider component

---
+++ frontend/providers/ThemeProvider.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React, { ReactNode } from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+type Props = {
+	children: ReactNode
+}
+
+const ThemeProvider = ({ children }: Props) => {
+	return <NextThemesProvider attribute='class' enableSystem defaultTheme='system'>{children}</NextThemesProvider>
+}
+
+export default ThemeProvider

14a2b20e6c26a9e579cd8731908eafb5784c8557
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 12:34:14 +0300

    Updated the imports for the templates on the component

--- frontend/components/Regions/RegionCards.tsx
+++ frontend/components/Regions/RegionCards.tsx
@@ -1,69 +1,39 @@
 'use client'
-
-import { selectAuthState } from '@/redux/features/auth/authSlice'
-import {
-	getAllRegions,
-	selectAllRegions
-} from '@/redux/features/regions/allRegionsSlice'
-import { useAppDispatch, useAppSelector } from '@/redux/hooks'
-import { useEffect } from 'react'
-import AddRegionCardTemplate from '../Templates/AddRegionCardTemplate'
-import CardTemplate from '../Templates/CardTemplate'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 import RegionCardTemplate from '../Templates/RegionCardTemplate'
+import AddRegionCardTemplate from '../Templates/AddRegionCardTemplate'
 
 type Props = {}
 
 const RegionCards = (props: Props) => {
-	const regionsData = useAppSelector(selectAllRegions)
-	const isLoggedIn = useAppSelector(selectAuthState)
-	const dispatch = useAppDispatch()
+	const regions: IRegion[] = []
 
 	useEffect(() => {
-		dispatch(getAllRegions())
+		const getRegionsData = async () => {
+			return (await axios.get('http://localhost:8000/regions')).data
+		}
+
+		const regions = getRegionsData()
 	}, [])
 
-	const content = isLoggedIn ? (
-		<div className='p-5 w-90 flex flex-wrap items-center justify-center gap-2'>
-			<>
-				{regionsData.map((region) => {
-					return (
-						<RegionCardTemplate
-							key={region.region_id}
-							title={region.region_name}
-							link={`regions/${region.region_id}`}
-							content={
-								'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam recusandae exercitationem repellat eum saepe iure ab similique provident laboriosam a maxime soluta asperiores, adipisci architecto tempore nisi molestias sit. Reprehenderit fugiat magni odio dicta earum.'
-							}
-							createdAt={region.created_at!}
-							regionId={region.region_id}
-						/>
-					)
-				})}
-				<AddRegionCardTemplate />
-			</>
-		</div>
-	) : (
-		<div className='p-5 w-90 flex flex-wrap items-center justify-center gap-2'>
-			<>
-				{regionsData.map((region) => {
-					return (
-						<CardTemplate
-							key={region.region_id}
-							title={region.region_name}
-							link={`regions/${region.region_id}`}
-							content={
-								'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam recusandae exercitationem repellat eum saepe iure ab similique provident laboriosam a maxime soluta asperiores, adipisci architecto tempore nisi molestias sit. Reprehenderit fugiat magni odio dicta earum.'
-							}
-							createdAt={region.created_at!}
-						/>
-					)
-				})}
-				<AddRegionCardTemplate />
-			</>
-		</div>
-	)
-
-	return content
+	return <>
+    {regions.map((region) => {
+        return (
+            <RegionCardTemplate
+                key={region.region_id}
+                title={region.region_name}
+                link={`regions/${region.region_id}`}
+                content={
+                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam recusandae exercitationem repellat eum saepe iure ab similique provident laboriosam a maxime soluta asperiores, adipisci architecto tempore nisi molestias sit. Reprehenderit fugiat magni odio dicta earum.'
+                }
+                createdAt={region.created_at!}
+                regionId={region.region_id}
+            />
+        )
+    })}
+    <AddRegionCardTemplate />
+</>
 }
 
 export default RegionCards

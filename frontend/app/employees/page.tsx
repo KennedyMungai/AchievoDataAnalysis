a927212074271cdfa0375105a82a8d31aaf56823
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 14:48:13 +0300

    Created the employees page

--- frontend/app/employees/page.tsx
+++ frontend/app/employees/page.tsx
@@ -1,3 +1,4 @@
+import EmployeesTable from '@/components/Employees/EmployeesTable'
 import TopBar from '@/components/TopBar/TopBar'
 import React from 'react'
 
@@ -7,6 +8,7 @@ const EmployeesPage = (props: Props) => {
 	return (
 		<main className='w-[94rem] min-h-screen bg-slate-200 dark:bg-slate-800 ml-[6vw]'>
 			<TopBar pageTitle={'Employees'} />
+			<EmployeesTable />
 		</main>
 	)
 }

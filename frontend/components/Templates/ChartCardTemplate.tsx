094482b1454ff0bdda24ac808ea606a13415ea6e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 12:01:07 +0300

    Created the ChartCardTemplate component

---
+++ frontend/components/Templates/ChartCardTemplate.tsx
@@ -0,0 +1,23 @@
+'use client'
+import React, { ReactNode } from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card'
+
+type Props = {
+    title: string
+    description: string
+    children: ReactNode
+}
+
+const ChartCardTemplate = ({title, description, children}: Props) => {
+  return (
+    <Card>
+			<CardHeader>
+				<CardTitle>{title}</CardTitle>
+				<CardDescription>{description}</CardDescription>
+			</CardHeader>
+			<CardContent>{children}</CardContent>
+		</Card>
+  )
+}
+
+export default ChartCardTemplate

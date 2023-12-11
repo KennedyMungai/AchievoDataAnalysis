4d3c534e6a0fc951b0b3dc006b573af7cc5ce24f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 13:49:29 +0300

    Created teh regionstores page

---
+++ frontend/app/stores/[regionId]/page.tsx
@@ -0,0 +1,18 @@
+import RegionStoresCards from '@/components/Regions/RegionStoresCards'
+
+type Props = {
+	params: {
+		regionStoreId: string
+	}
+}
+
+
+const RegionStoresPage = ({params: {regionStoreId}}: Props) => {
+	return (
+		<div className='min-h-screen ml-[5rem] bg-slate-100 dark:bg-slate-800'>
+            <RegionStoresCards regionId={Number(regionStoreId)} />
+		</div>
+	)
+}
+
+export default RegionStoresPage

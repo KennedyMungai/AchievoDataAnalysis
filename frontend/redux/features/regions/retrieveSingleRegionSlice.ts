804335bcad14adefc18f1e94e008ea4975ef0412
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 13:10:58 +0300

    Created the slice to retrieve a single region

---
+++ frontend/redux/features/regions/retrieveSingleRegionSlice.ts
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+export const retrieveSingleRegion = createAsyncThunk('regions/singleRegion', async (regionId: number) => {
+    const response = await axios.get(`http://localhost:8000/{region_id}`)
+
+    const regionData = response.data
+
+    return regionData
+})
+
+
+const initialState: IRegionData = {
+    is_loaded: false,
+    region_id: 0,
+    region_name: "",
+    created_at: new Date()
+}
+
+
+const retrieveSingleRegionSlice = createSlice({
+    name: 'regions/singleRegion',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(retrieveSingleRegion.pending, (state) => {
+                state.is_loaded = false
+            })
+            .addCase(retrieveSingleRegion.fulfilled, (state, action:PayloadAction<IRegion>) => {
+                state.is_loaded = true
+                state.region_id = action.payload.region_id
+                state.region_name = action.payload.region_name
+                state.created_at = action.payload.created_at
+            })
+            .addCase(retrieveSingleRegion.rejected, (state) => {
+                state.is_loaded = false
+            })
+    }
+})
+
+
+export default retrieveSingleRegionSlice.reducer

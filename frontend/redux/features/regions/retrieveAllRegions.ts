f3541c7b4e978729fc09f77b004faac3b8d96ec6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 12:54:42 +0300

    Created the slice to retrieve all regions

---
+++ frontend/redux/features/regions/retrieveAllRegions.ts
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const retrieveAllRegions = createAsyncThunk("regions/allRegions", async () => {
+    const response = await axios.get("http://localhost:8000/regions")
+
+    const regions = response.data
+
+    return regions
+})
+
+const initialState: IRegionsData = {
+    regions: [],
+    is_loaded: false
+}
+
+
+const retrieveRegionsSlice = createSlice({
+    name: 'Retrieve All Regions',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(retrieveAllRegions.pending, (state) => {
+                state.is_loaded = false
+                state.regions = []
+            })
+            .addCase(retrieveAllRegions.fulfilled, (state, action: PayloadAction<IRegion[]>) => {
+                state.regions = action.payload
+            })
+            .addCase(retrieveAllRegions.rejected, (state) => {
+                state.is_loaded = false
+                state.regions = []
+            })
+    }
+})
+
+export default retrieveRegionsSlice.reducer

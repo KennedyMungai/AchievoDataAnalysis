9ac8cbbf467e548629edf8529ae4fff9398218d8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 12:00:12 +0300

    Created the DashboardCards component

---
+++ frontend/components/Templates/DashboardCards.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+type Props = {
+	title: string
+	value: number
+}
+
+const DashboardCards = ({ title, value }: Props) => {
+	return (
+		<Card className='w-[20rem]'>
+			<CardHeader>
+				<CardTitle className='text-md text-center'>{title}</CardTitle>
+			</CardHeader>
+			<CardContent className='text-center'>Ksh {value}</CardContent>
+		</Card>
+	)
+}
+
+export default DashboardCards

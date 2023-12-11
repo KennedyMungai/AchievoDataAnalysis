772411061c60fd5fce11ef642e181530ef532bd2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 12:26:52 +0300

    Cleared some of the logic

--- frontend/app/regions/page.tsx
+++ frontend/app/regions/page.tsx
@@ -1,13 +1,15 @@
+import RegionCards from '@/components/Regions/RegionCards'
 import TopBar from '@/components/TopBar/TopBar'
-import React from 'react'
 
 type Props = {}
 
 const RegionsPage = (props: Props) => {
-	return <main  className="w-[94vw] min-h-screen bg-slate-200 dark:bg-slate-800 ml-[6vw]">
-        <TopBar pageTitle={'Regions'} />
-        {/* TODO: Add the regions cards component */}
-    </main>
+	return (
+		<main className='w-[94vw] min-h-screen bg-slate-200 dark:bg-slate-800 ml-[6vw]'>
+			<TopBar pageTitle={'Regions'} />
+			<RegionCards />
+		</main>
+	)
 }
 
 export default RegionsPage

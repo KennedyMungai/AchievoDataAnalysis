e7578435858a3006a2e6909aea104e751e6dfbf5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 14:45:42 +0300

    Created the DataTable component

---
+++ frontend/components/Employees/DataTable.tsx
@@ -0,0 +1,181 @@
+'use client'
+
+import {
+	ColumnDef,
+	ColumnFiltersState,
+	SortingState,
+	VisibilityState,
+	flexRender,
+	getCoreRowModel,
+	getFilteredRowModel,
+	getPaginationRowModel,
+	getSortedRowModel,
+	useReactTable
+} from '@tanstack/react-table'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow
+} from '../ui/table'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuTrigger
+} from '../ui/dropdown-menu'
+import { useState } from 'react'
+import {
+	Sheet,
+	SheetClose,
+	SheetContent,
+	SheetDescription,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger
+} from '../ui/sheet'
+
+interface DataTableProps<TData, TValue> {
+	columns: ColumnDef<TData, TValue>[]
+	data: TData[]
+}
+
+export function DataTable<TData, TValue>({
+	columns,
+	data
+}: DataTableProps<TData, TValue>) {
+	const [sorting, setSorting] = useState<SortingState>([])
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+	const [columnVisibility, setColumnVisibility] = useState<VisibilityState>(
+		{}
+	)
+	const [rowSelection, setRowSelection] = useState({})
+
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+		getFilteredRowModel: getFilteredRowModel(),
+
+		onSortingChange: setSorting,
+		onColumnFiltersChange: setColumnFilters,
+		onColumnVisibilityChange: setColumnVisibility,
+		onRowSelectionChange: setRowSelection,
+		state: {
+			sorting,
+			columnFilters,
+			columnVisibility,
+			rowSelection
+		}
+	})
+
+	return (
+		<div className=''>
+			<div className='flex items-center py-4 justify-between'>
+				<Input
+					placeholder='Filter First Names'
+					value={
+						(table
+							.getColumn('first_name')
+							?.getFilterValue() as string) || ''
+					}
+					onChange={(e) =>
+						table
+							.getColumn('first_name')
+							?.setFilterValue(e.target.value)
+					}
+					className='max-w-sm'
+				/>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant={'outline'}>Columns</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align='end'>
+						{table
+							.getAllColumns()
+							.filter((column) => column.getCanHide())
+							.map((column) => (
+								<DropdownMenuCheckboxItem
+									checked={column.getIsVisible()}
+									onCheckedChange={(value) =>
+										column.toggleVisibility(!!value)
+									}
+									key={column.id}
+									className='capitalize'
+								>
+									{column.id}
+								</DropdownMenuCheckboxItem>
+							))}
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
+			<div className='rounded-md border dark:border-slate-400'>
+				<Table>
+					<TableHeader>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((header) => (
+									<TableHead key={header.id}>
+										{flexRender(
+											header.column.columnDef.header,
+											header.getContext()
+										)}
+									</TableHead>
+								))}
+							</TableRow>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map((row) => (
+								<TableRow key={row.id}>
+									{row.getVisibleCells().map((cell) => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext()
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell>No Data Found</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>
+			<div className='flex items-center justify-end space-x-2 py-4'>
+				<Button
+					variant={'outline'}
+					size={'sm'}
+					onClick={() => table.previousPage()}
+					disabled={!table.getCanPreviousPage()}
+				>
+					Prev
+				</Button>
+				<Button
+					variant={'outline'}
+					size={'sm'}
+					onClick={() => table.nextPage()}
+					disabled={!table.getCanNextPage()}
+				>
+					Next
+				</Button>
+			</div>
+			<div className='flex-1 text-sm text-muted-foreground'>
+				{table.getFilteredSelectedRowModel().rows.length} of{' '}
+				{table.getFilteredRowModel().rows.length} rows selected
+			</div>
+		</div>
+	)
+}

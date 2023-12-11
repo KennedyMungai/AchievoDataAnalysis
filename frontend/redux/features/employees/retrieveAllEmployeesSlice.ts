8df1a2ddb83e08ebba97c3cfafe030874bfe6b06
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 14:38:22 +0300

    Created the slice to retrieve all employyes

---
+++ frontend/redux/features/employees/retrieveAllEmployeesSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+export const retrieveAllEmployees = createAsyncThunk("employees/allEmployees", async () => {
+    const response = await axios.get("http://localhost:8000/employees")
+
+    const employees = response.data
+
+    return employees
+})
+
+
+const initialState: IEmployeesData = {
+    employees: [],
+    is_loaded: false
+}
+
+
+const retrieveAllEmployeesSlice = createSlice({
+    name: "employees/allEmployees",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(retrieveAllEmployees.pending, (state) => {
+                state.is_loaded = false
+            })
+            .addCase(retrieveAllEmployees.fulfilled, (state, action: PayloadAction<IEmployee[]>) => {
+                state.is_loaded = true
+                state.employees = action.payload
+            })
+            .addCase(retrieveAllEmployees.rejected, (state) => {
+                state.is_loaded = false
+            })
+    }
+})
+
+// export const selectAllEmployees = (state: RootState) => state.allEmployees.employees
+
+export default retrieveAllEmployeesSlice.reducer

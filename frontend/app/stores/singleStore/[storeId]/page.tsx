d81d04402d4a3e5c5b1a3b5bf36156418959bdef
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 15:34:35 +0300

    Createdthe single store page

--- frontend/app/stores/singleStore/[storeId]/page.tsx
+++ frontend/app/stores/singleStore/[storeId]/page.tsx
@@ -1,11 +1,29 @@
+import DashboardTemplate from '@/components/Templates/DashboardTemplate'
 import React from 'react'
 
 type Props = {}
 
 const SingleStorePage = (props: Props) => {
-  return (
-    <div>SingleStorePage</div>
-  )
+	return (
+		<div className='min-h-screen ml-[5rem] bg-slate-100 dark:bg-slate-800'>
+			<DashboardTemplate
+				title={''}
+				buttonName={''}
+				dashboardCard1Value={0}
+				dashboardCard1Title={''}
+				dashboardCard2Value={0}
+				dashboardCard2Title={''}
+				dashboardCard3Value={0}
+				dashboardCard3Title={''}
+				dashboardCard4Value={0}
+				dashboardCard4Title={''}
+				chartCardTitle={''}
+				chartCardDescription={''}
+				chartCardContent={undefined}
+				scrollAreaComponent={undefined}
+			/>
+		</div>
+	)
 }
 
 export default SingleStorePage

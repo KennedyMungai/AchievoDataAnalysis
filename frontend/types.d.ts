edd6f4c9bddba3e2a7d38cc21065e345e7389060
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 12:50:41 +0300

    Added two interfaces to the types file

--- frontend/types.d.ts
+++ frontend/types.d.ts
@@ -1,23 +1,27 @@
-interface IToken
-{
+interface IToken {
     token_type: string
     access_token: string
     is_loaded: boolean
     is_logged_in: boolean
 }
 
-interface ICredentials
-{
+interface ICredentials {
     employee_email: string,
     employee_password: string
 }
 
-interface IRegion
-{
+interface IRegion {
     region_id: number,
     region_name: string,
     created_at: Date,
     updated_at?: Date
+}
 
+interface IRegionData extends IRegion {
+    is_loaded: boolean
+}
+
+interface IRegionsData {
+    regions: IRegion[],
     is_loaded: boolean
 }

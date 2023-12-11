f126aac5a05ac01bf1214d3ba1fbe22c9597e236
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 13:04:41 +0300

    Created the page for the individual regions

---
+++ frontend/app/regions/[regionId]/page.tsx
@@ -0,0 +1,36 @@
+'use client'
+import DashboardTemplate from '@/components/Templates/DashboardTemplate'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import React, { useEffect } from 'react'
+
+type Props = {
+	params: {
+		regionId: string
+	}
+}
+
+const SingleRegionPage = ({ params: { regionId } }: Props) => {
+	return (
+		<div className='min-h-screen ml-[5rem] bg-slate-100 dark:bg-slate-800'>
+			<DashboardTemplate
+				title={'Some Region Name'}
+				buttonLink={`stores/${regionId}`}
+				buttonName={'Stores'}
+				dashboardCard1Value={538}
+				dashboardCard1Title={'No. Of Incidents'}
+				dashboardCard2Value={536997}
+				dashboardCard2Title={'Value Of Incidents'}
+				dashboardCard3Value={5632}
+				dashboardCard3Title={'Value of Incidents from top store'}
+				dashboardCard4Value={51151}
+				dashboardCard4Title={'Value of Most Recurring Incident'}
+				chartCardTitle={'Some Card Title'}
+				chartCardDescription={'Some Card Description'}
+				chartCardContent={undefined}
+				scrollAreaComponent={undefined}
+			/>
+		</div>
+	)
+}
+
+export default SingleRegionPage

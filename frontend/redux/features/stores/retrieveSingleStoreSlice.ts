2d3fd9b08ea0d395a4dfb335be2a3086d1c560fd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-11 15:42:31 +0300

    Created the retriveStoreSlice

---
+++ frontend/redux/features/stores/retrieveSingleStoreSlice.ts
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit"
+import axios from "axios"
+
+
+export const retrieveSingleStore = createAsyncThunk("stores/singleStore", async (storeId) => {
+    const response = await axios.get(`http://localhost:8000/stores/${storeId}`)
+    const data = response.data
+    return data
+})
+
+
+const initialState: IStoreData = {
+    is_loaded: false,
+    store_name: "",
+    store_location: "",
+    created_at: new Date,
+    region_id: 0,
+    store_id: 0
+}
+
+
+const retrieveSingleStoreSlice = createSlice({
+    name: "stores/singleStore",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(retrieveSingleStore.pending, (state) => {
+                state.is_loaded = false
+            })
+            .addCase(retrieveSingleStore.fulfilled, (state, action: PayloadAction<IStoreData>) => {
+                state.is_loaded
+                state.store_name = action.payload.store_name
+                state.store_location = action.payload.store_location
+                state.created_at = action.payload.created_at
+                state.region_id = action.payload.region_id
+                state.store_id = action.payload.store_id
+            })
+            .addCase(retrieveSingleStore.rejected, (state) => {
+                state.is_loaded = false
+            })
+    }
+})
+
+
+// export const selectSingleStore = (state: RootState) => 
+
+export default retrieveSingleStoreSlice.reducer

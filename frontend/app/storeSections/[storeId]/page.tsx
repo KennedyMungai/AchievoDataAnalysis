f32e5c0204948e9164829cc23fc8a224f92012e8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-13 14:55:12 +0300

    Changed something

--- frontend/app/storeSections/[storeId]/page.tsx
+++ frontend/app/storeSections/[storeId]/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 import StoreSectionCards from '@/components/StoreSections/StoreSectionCards'
 import TopBar from '@/components/TopBar/TopBar'
-import { retrieveSingleStoreSection, selectSingleStoreSection } from '@/redux/features/storeSections/retrieveSingleStoreSectionSlice'
+import {
+	retrieveSingleStoreSection,
+	selectSingleStoreSection
+} from '@/redux/features/storeSections/retrieveSingleStoreSectionSlice'
 import { selectSingleStore } from '@/redux/features/stores/retrieveSingleStoreSlice'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import { useEffect } from 'react'
@@ -12,14 +15,13 @@ type Props = {
 	}
 }
 
-const StoreSectionsPage = ({params: {storeId}}: Props) => {
+const StoreSectionsPage = ({ params: { storeId } }: Props) => {
 	const storeData = useAppSelector(selectSingleStore)
 	const dispatch = useAppDispatch()
 
 	useEffect(() => {
 		dispatch(retrieveSingleStoreSection(Number(storeId)))
 	}, [])
-	
 
 	return (
 		<div className='min-h-screen ml-[5rem] bg-slate-100 dark:bg-slate-800'>

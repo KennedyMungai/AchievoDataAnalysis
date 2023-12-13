f6b332300f11f6d09fd4f0a52217d725b53c3d90
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-13 14:15:19 +0300

    Created the store section cards component

---
+++ frontend/components/StoreSections/StoreSectionCards.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+
+type Props = {}
+
+const StoreSectionCards = (props: Props) => {
+  return (
+    <div className='p-5 w-90 flex flex-wrap items-center justify-center gap-2'>
+        
+    </div>
+  )
+}
+
+export default StoreSectionCards

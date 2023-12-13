0b9af785ad76533dc309d69d01e278b5f985279c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-13 10:21:45 +0300

    Added tghe redux functions for the app

--- frontend/app/storeSections/[storeId]/page.tsx
+++ frontend/app/storeSections/[storeId]/page.tsx
@@ -1,9 +1,26 @@
+'use client'
 import DashboardTemplate from '@/components/Templates/DashboardTemplate'
-import React from 'react'
+import { retrieveSingleStoreSection, selectSingleStoreSection } from '@/redux/features/storeSections/retrieveSingleStoreSectionSlice'
+import { selectSingleStore } from '@/redux/features/stores/retrieveSingleStoreSlice'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import React, { useEffect } from 'react'
 
-type Props = {}
+type Props = {
+	params: {
+		storeId: string
+	}
+}
 
-const StoreSectionsPage = (props: Props) => {
+const StoreSectionsPage = ({params: {storeId}}: Props) => {
+	const storeSectionData = useAppSelector(selectSingleStoreSection)
+	const storeData = useAppSelector(selectSingleStore)
+	const dispatch = useAppDispatch()
+
+	useEffect(() => {
+		dispatch(retrieveSingleStoreSection(Number(storeId)))
+	}, [])
+	
+
 	return (
 		<div className='min-h-screen ml-[5rem] bg-slate-100 dark:bg-slate-800'>
 			<DashboardTemplate

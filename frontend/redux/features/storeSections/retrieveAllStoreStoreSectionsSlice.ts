318ff6df0fec36e8ead27a2c05eb57a42e0eb7b7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-13 12:50:05 +0300

    Wrote the slice to retrieve all of a stores section

---
+++ frontend/redux/features/storeSections/retrieveAllStoreStoreSectionsSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit"
+import axios from "axios"
+
+
+export const retrieveAllOfAStoresStoreSections = createAsyncThunk("storeSections/retrieveAllStoreSections", async (storeId: number) => {
+    const response = await axios.get(`http://localhost:8000/store-sections/${storeId}`)
+    const data = response.data
+    return data
+})
+
+
+const initialState: IStoreSectionsData = {
+    store_sections: [],
+    is_loaded: false
+}
+
+
+const retrieveAllStoreSectionsSlice = createSlice({
+    name: "storeSections/allStoreSections",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(retrieveAllOfAStoresStoreSections.pending, (state) => {
+                state.is_loaded = false
+            })
+            .addCase(retrieveAllOfAStoresStoreSections.fulfilled, (state, action:PayloadAction<IStoreSection[]>) => {
+                state.is_loaded = true
+                state.store_sections = action.payload
+            })
+            .addCase(retrieveAllOfAStoresStoreSections.rejected, (state) => {
+                state.is_loaded = false
+            })
+    }
+})
+
+
+// export const selectAllStoreSections = (state: RootState) => state.allStoreSections.store_sections
+
+export default retrieveAllStoreSectionsSlice.reducer

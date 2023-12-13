0e4891dbaf9affb0ac7a6e3667086938e3227e0f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-13 11:40:21 +0300

    Created the single store section page

---
+++ frontend/app/storeSections/singleStoreSection/[storeSectionId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const SingleStoreSectionPage = (props: Props) => {
+	return <div>SingleStoreSectionPage</div>
+}
+
+export default SingleStoreSectionPage

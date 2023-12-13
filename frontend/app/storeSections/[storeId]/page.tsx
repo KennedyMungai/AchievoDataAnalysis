0a0526d061cfc14b2b66bf76d9357e03360c53fb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-13 09:46:18 +0300

    Added a dashboatd for the store sections

--- frontend/app/storeSections/[storeId]/page.tsx
+++ frontend/app/storeSections/[storeId]/page.tsx
@@ -1,9 +1,29 @@
+import DashboardTemplate from '@/components/Templates/DashboardTemplate'
 import React from 'react'
 
 type Props = {}
 
 const StoreSectionsPage = (props: Props) => {
-	return <div>StoreSectionsPage</div>
+	return (
+		<div className='min-h-screen ml-[5rem] bg-slate-100 dark:bg-slate-800'>
+			<DashboardTemplate
+				title={'Store Section Name'}
+				buttonName={'Add Incident'}
+				dashboardCard1Value={8500}
+				dashboardCard1Title={'Some Value'}
+				dashboardCard2Value={7859}
+				dashboardCard2Title={'Some Other Value'}
+				dashboardCard3Value={78542}
+				dashboardCard3Title={'Another Value'}
+				dashboardCard4Value={7878}
+				dashboardCard4Title={'Different Value'}
+				chartCardTitle={'Store Section Data'}
+				chartCardDescription={'Store Section Data Described'}
+				chartCardContent={undefined}
+				scrollAreaComponent={undefined}
+			/>
+		</div>
+	)
 }
 
 export default StoreSectionsPage

18d98b5fb669ca37012c29925cd7f1131e665504
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-19 09:00:47 +0300

    Created the TopIncidentCard componet

---
+++ frontend/components/Stores/TopIncidentCard.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+type Props = {}
+
+const TopIncidentCard = (props: Props) => {
+	return <div className="font-semibold text-muted bg-slate-300 dark:bg-slate-600 my-2 p-2">TopIncidentCard</div>
+}
+
+export default TopIncidentCard

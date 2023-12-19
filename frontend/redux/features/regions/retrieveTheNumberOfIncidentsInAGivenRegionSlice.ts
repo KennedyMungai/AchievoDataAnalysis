320ca4b724c8d5f0afe3eef147b4d92029a01d35
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-19 15:01:05 +0300

    Created the slice to retrieve the number of incidents in a given region

---
+++ frontend/redux/features/regions/retrieveTheNumberOfIncidentsInAGivenRegionSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit"
+import axios from "axios"
+
+
+export const retrieveTheNumberOfIncidentsInAGivenRegion = createAsyncThunk("regions/count", async (regionId: number) => {
+    const response = await axios.get(`http://localhost:8000/incidents/region/count/${regionId}`)
+    const data = response.data
+    return data
+})
+
+
+const initialState: ICountData = {
+    is_loaded: false,
+    count: 0
+}
+
+
+export const retrieveTheNumberOfIncidentsInAGivenRegionSlice = createSlice({
+    name: 'regions/count',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(retrieveTheNumberOfIncidentsInAGivenRegion.pending, (state) => {
+                state.is_loaded = false
+            })
+            .addCase(retrieveTheNumberOfIncidentsInAGivenRegion.fulfilled, (state, action: PayloadAction<ICount>) => {
+                state.is_loaded = true
+                state.count = action.payload.count
+            })
+            .addCase(retrieveTheNumberOfIncidentsInAGivenRegion.rejected, (state) => {
+                state.is_loaded = false
+            })
+    }
+})
+
+
+export default retrieveTheNumberOfIncidentsInAGivenRegionSlice.reducer

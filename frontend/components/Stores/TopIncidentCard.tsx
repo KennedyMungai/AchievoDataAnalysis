f2f7bf3e844cbc767d867bd2d5fbc6416480911c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-19 09:15:22 +0300

    Added styles to the card component

--- frontend/components/Stores/TopIncidentCard.tsx
+++ frontend/components/Stores/TopIncidentCard.tsx
@@ -1,9 +1,17 @@
 'use client'
 
-type Props = {}
+import moment from "moment"
 
-const TopIncidentCard = (props: Props) => {
-	return <div className="font-semibold text-muted bg-slate-300 dark:bg-slate-600 my-2 p-2">TopIncidentCard</div>
+type Props = {
+    incident: IIncident
+}
+
+const TopIncidentCard = ({incident}: Props) => {
+	return <div className="font-semibold bg-slate-300 dark:bg-slate-600 my-2 p-2 rounded-md flex justify-between">
+        <span className="float-right">{moment(incident.created_at).format("HH:mm:ss")}</span>
+        <span className="text-left">{incident.incident_description}</span>
+        <span>KSH {incident.total_value}</span>
+    </div>
 }
 
 export default TopIncidentCard

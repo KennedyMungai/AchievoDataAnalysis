efe123f55014e25d187a89e3e8bcfa4fc01c40b4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-19 11:50:09 +0300

    Created the date slice

---
+++ frontend/redux/features/date/dateSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit"
+import { RootState } from "../store"
+
+const initialState = {
+    date: new Date()
+}
+
+export const dateSlice = createSlice({
+    name: 'date',
+    initialState,
+    reducers: {
+        setGlobalDate: (state, action) => {
+            state.date = action.payload
+        }
+    }
+})
+
+export const {setGlobalDate} = dateSlice.actions
+
+export const selectDate = (state: RootState) => state.dateState
+
+export default dateSlice.reducer

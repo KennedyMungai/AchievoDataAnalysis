fe0118373cab4fef6fd1a8e8ef7745c9dbffeefa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-19 14:36:47 +0300

    The slice for retrieving the top twenty incidents in a region

---
+++ frontend/redux/features/regions/retrieveTheTopTwentyIncidentsInARegionSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit"
+import axios from "axios"
+import { RootState } from "../store"
+
+
+
+export const retrieveTheTopTwentyIncidentsInARegion = createAsyncThunk('region/topTwenty', async (regionId) => {
+    const response = await axios.get(`http://localhost:8000/incidents/region/top_twenty/${regionId}`)
+    const data = response.data
+    return data
+})
+
+
+const initialState: IIncidentsData = {
+    incidents: [],
+    is_loaded: false
+}
+
+
+export const retrieveTheTopTwentyIncidentsInARegionSlice = createSlice({
+    name: 'region/topTwenty',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(retrieveTheTopTwentyIncidentsInARegion.pending, (state) => {
+                state.is_loaded = false
+            })
+            .addCase(retrieveTheTopTwentyIncidentsInARegion.fulfilled, (state, action: PayloadAction<IIncident[]>) => {
+                state.is_loaded = true
+                state.incidents = action.payload
+            })
+            .addCase(retrieveTheTopTwentyIncidentsInARegion.rejected, (state) => {
+                state.is_loaded = false
+            })
+    }
+})
+
+export const selectTopTwentyIncidentsInARegion = (state: RootState) => state.theTopTwentyIncidentsInARegion.incidents
+
+export default retrieveTheTopTwentyIncidentsInARegionSlice.reducer

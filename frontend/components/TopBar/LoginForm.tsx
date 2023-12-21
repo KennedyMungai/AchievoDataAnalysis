6d734749e8609eb680706c167e35d2e220a07c1f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-21 11:46:10 +0300

    Updated some outdated logic

--- frontend/components/TopBar/LoginForm.tsx
+++ frontend/components/TopBar/LoginForm.tsx
@@ -1,5 +1,10 @@
 'use client'
-import { getAuthToken, logIn, logOut, selectAuthStateData } from '@/redux/features/auth/authSlice'
+import {
+	getAuthToken,
+	logIn,
+	logOut,
+	selectAuthStateData
+} from '@/redux/features/auth/authSlice'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
@@ -41,10 +46,11 @@ const formSchema = z.object({
 })
 
 const LoginButton = (props: Props) => {
-    // TODO: Recheck the logic used to authenticate users
+	// TODO: Recheck the logic used to authenticate users
 
 	const dispatch = useAppDispatch()
-	const {employee_name, is_logged_in: isLoggedIn} = useAppSelector(selectAuthStateData)
+	const { employee_name, is_logged_in: isLoggedIn } =
+		useAppSelector(selectAuthStateData)
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
@@ -54,15 +60,22 @@ const LoginButton = (props: Props) => {
 		}
 	})
 
-	const onSubmit = async ({userEmail, password}: z.infer<typeof formSchema>) => {
+	const onSubmit = async ({
+		userEmail,
+		password
+	}: z.infer<typeof formSchema>) => {
 		try {
-			dispatch(getAuthToken({employee_email: userEmail, employee_password: password}))
+			dispatch(
+				getAuthToken({
+					employee_email: userEmail,
+					employee_password: password
+				})
+			)
 			dispatch(logIn())
 			toast.success('Successfully Logged in')
 		} catch (error) {
 			toast.error('Something went wrong')
 		}
-		
 
 		form.reset()
 		form.setValue('userEmail', '')
@@ -74,94 +87,24 @@ const LoginButton = (props: Props) => {
 		dispatch(logOut())
 
 		toast.success('Successfully Logged out')
-	}	
-
-	if (!isLoggedIn) {
-		return (
-			<Dialog open={!isLoggedIn}>
-				<DialogTrigger asChild>
-					<Button variant={'outline'}>Login</Button>
-				</DialogTrigger>
-				<DialogContent>
-					<DialogHeader>
-						<DialogTitle>Login</DialogTitle>
-						<DialogDescription>
-							Please enter your email address and Password
-						</DialogDescription>
-					</DialogHeader>
-					<Form {...form}>
-						<form
-							onSubmit={form.handleSubmit(onSubmit)}
-							className='space-y-8'
-						>
-							<FormField
-								control={form.control}
-								name='userEmail'
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Email</FormLabel>
-										<FormControl>
-											<Input
-												{...field}
-												placeholder='Email'
-											/>
-										</FormControl>
-										<FormDescription>
-											Please enter your email address
-										</FormDescription>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name='password'
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Password</FormLabel>
-										<FormControl>
-											<Input
-												{...field}
-												placeholder='Password'
-												type='password'
-											/>
-										</FormControl>
-										<FormDescription>
-											Please enter your password
-										</FormDescription>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<DialogFooter>
-								<DialogClose asChild>
-									<Button type='submit'>Cancel</Button>
-								</DialogClose>
-								<Button variant='outline'>Submit</Button>
-							</DialogFooter>
-						</form>
-					</Form>
-				</DialogContent>
-			</Dialog>
-		)
 	}
 
-	if (isLoggedIn) {
-		return (
-			<DropdownMenu>
-				<DropdownMenuTrigger>
-					<Button variant={'outline'}>Welcome Back {employee_name}</Button>
-				</DropdownMenuTrigger>
-				<DropdownMenuContent>
-					<DropdownMenuItem className='flex justify-center'>
-						<Button variant={'destructive'} onClick={logOutHandler}>
-							Logout
-						</Button>
-					</DropdownMenuItem>
-				</DropdownMenuContent>
-			</DropdownMenu>
-		)
-	}
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<Button variant={'outline'}>
+					Welcome Back {employee_name}
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuItem className='flex justify-center'>
+					<Button variant={'destructive'} onClick={logOutHandler}>
+						Logout
+					</Button>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
 }
 
 export default LoginButton

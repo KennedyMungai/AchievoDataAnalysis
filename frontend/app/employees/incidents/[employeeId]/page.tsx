6f408d8b3d15008bdf9d41efabc229109de64b36
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-12-21 09:51:53 +0300

    Created the employees incidents dashboard component page

---
+++ frontend/app/employees/incidents/[employeeId]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+	params: {
+		employeeId: string
+	}
+}
+
+const EmployeeIncidentsPage = (props: Props) => {
+	return <div className='min-h-screen ml-[5rem] bg-slate-100 dark:bg-slate-800 overflow-x-hidden'>EmployeeIncidentsPage</div>
+}
+
+export default EmployeeIncidentsPage

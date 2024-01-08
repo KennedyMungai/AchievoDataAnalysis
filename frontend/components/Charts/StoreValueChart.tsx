7e74f166d9d57c9c2424a4b336ec5b4466da3a95
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-08 14:24:19 +0300

    Made the chart component map out actual content from the backend

--- frontend/components/Charts/StoreValueChart.tsx
+++ frontend/components/Charts/StoreValueChart.tsx
@@ -2,6 +2,10 @@
 import Chart from 'chart.js/auto'
 import { CategoryScale } from 'chart.js'
 import { Line } from 'react-chartjs-2'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import { retrieveTheStoreGraphingData, selectTheGraphingData, selectTheGraphingDataLabels } from '@/redux/features/stores/retrieveTheStoreGraphingDataSlice'
+import { useEffect } from 'react'
+
 
 type Props = {
 	storeId: number
@@ -10,12 +14,21 @@ type Props = {
 Chart.register(CategoryScale)
 
 const StoreValueChart = ({ storeId }: Props) => {
-	const data = {
-		labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+	const dispatch = useAppDispatch()
+	const data = useAppSelector(selectTheGraphingData)
+	const labels = useAppSelector(selectTheGraphingDataLabels)
+
+	useEffect(() => {
+		dispatch(retrieveTheStoreGraphingData(storeId))
+	}, [])
+	
+
+	const graphingData = {
+		labels,
 		datasets: [
 			{
 				label: 'Store Sections $',
-				data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
+				data,
 				borderColor: 'rgb(53, 162, 235)',
 				backgroundColor: 'rgb(53, 162, 235, 0.4'
 			}
@@ -37,7 +50,7 @@ const StoreValueChart = ({ storeId }: Props) => {
 
 	return (
 		<div className='lg:w-full w-[80vw] border rounded-lg bg-white h-[60vh] dark:bg-slate-900 p-5 flex items-center justify-center'>
-			<Line options={options} data={data} />
+			<Line options={options} data={graphingData} />
 		</div>
 	)
 }

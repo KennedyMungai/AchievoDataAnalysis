4749b6516de4c4f68bc43af04102d8944c12aaad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-08 09:22:29 +0300

    Deleted the references to the average slices to the store

--- frontend/redux/features/store.ts
+++ frontend/redux/features/store.ts
@@ -3,32 +3,27 @@ import authReducer from './auth/authSlice'
 import dateReducer from './date/dateSlice'
 import retrieveAllEmployeesReducer from './employees/retrieveAllEmployeesSlice'
 import retrieveSingleEmployeeReducer from './employees/retrieveSingleEmployeesSlice'
-import retrieveTheAverageValueOfIncidentsReportedByAnEmployeeReducer from './employees/retrieveTheAverageValueOfIncidentsReportedByAnEmployeeSlice'
 import retrieveTheMostNotoriousIncidentReportedByAnEmployeeReducer from './employees/retrieveTheMostNotoriousIncidentReportedByAnEmployeeSlice'
 import retrieveTheNumberOfIncidentsReportedByAnEmployeeReducer from './employees/retrieveTheNumberOfIncidentsReportedByAnEmployeeSlice'
 import retrieveTheTenMostNotoriousIncidentReportedByAnEmployeeReducer from './employees/retrieveTheTenMostNotoriousIncidentReportedByAnEmployeeSlice'
 import retrieveTheValueOfIncidentsReportedByAnEmployee from './employees/retrieveTheValueOfIncidentsReportedByAnEmployeeSlice'
-import retrieveTheAverageValueOfOverallIncidentsReducer from './overall/retrieveTheAverageValueOfOverallIncidentsSlice'
 import retrieveTheMostNotoriousRegionReducer from './overall/retrieveTheMostNotoriousRegionSlice'
 import retrieveTheNumberOfOverallIncidentsReducer from './overall/retrieveTheNumberOfOverallIncidentsSlice'
 import retrieveTheTopTwentyMostNotoriousOverallIncidentsReducer from './overall/retrieveTheTopTwentyMostNotoriousOverallIncidentsSlice'
 import retrieveTheValueOfOverallIncidentsReducer from './overall/retrieveTheValueOfOverallIncidentsSlice'
 import retrieveAllRegionsReducer from './regions/retrieveAllRegionsSlice'
 import retrieveSingleRegionReducer from './regions/retrieveSingleRegionSlice'
-import retrieveTheAverageValueOfAllIncidentsInARegionReducer from './regions/retrieveTheAverageValueOfAllIncidentsInARegionSlice'
 import retrieveTheMostNotoriousStoreInARegionReducer from './regions/retrieveTheMostNotoriousStoreInARegionSlice'
 import retrieveTheNumberOfIncidentsInAGivenRegionReducer from './regions/retrieveTheNumberOfIncidentsInAGivenRegionSlice'
 import retrieveTheTopTwentyIncidentsInARegionReducer from './regions/retrieveTheTopTwentyIncidentsInARegionSlice'
 import retrieveTheTotalValueOfAllIncidentsInARegionReducer from './regions/retrieveTheValueOfAllIncidentsInARegionSlice'
 import retrieveAllStoreStoreSectionsReducer from './storeSections/retrieveAllStoreStoreSectionsSlice'
 import retrieveSingleStoreSectionReducer from './storeSections/retrieveSingleStoreSectionSlice'
-import retrieveTheAverageValueOfAllTheIncidentsInAStoreSectionReducer from './storeSections/retrieveTheAverageValueOfAllTheIncidentsInAStoreSectionSlice'
 import retrieveTheMostNotoriousIncidentInAStoreSectionReducer from './storeSections/retrieveTheMostNotoriousIncidentInAStoreSectionSlice'
 import retrieveTheNumberOfAllTheIncidentsInAStoreSectionReducer from './storeSections/retrieveTheNumberOfAllTheIncidentsInAStoreSectionSlice'
 import retrieveTheTopTwentyIncidentsInAStoreSectionReducer from './storeSections/retrieveTheTopTwentyIncidentsInAStoreSectionSlice'
 import retrieveTheValueOfAllIncidentsInAStoreSectionReducer from './storeSections/retrieveTheValueOfAllTheIncidentsInAStoreSectionSlice'
 import retrieveAllRegionStoresReducer from './stores/retrieveAllRegionStoresSlice'
-import retrieveSingleStoreAverageIncidentValueReducer from './stores/retrieveSingleStoreIncidentsAverageSlice'
 import retrieveSingleStoreIncidentsCountReducer from './stores/retrieveSingleStoreIncidentsCountSlice'
 import retrieveSingleStoreIncidentsValueReducer from './stores/retrieveSingleStoreIncidentsValueSlice'
 import retrieveSingleStoreReducer from './stores/retrieveSingleStoreSlice'
@@ -48,29 +43,24 @@ const store = configureStore({
         allStoreStoreSections: retrieveAllStoreStoreSectionsReducer,
         singleStoreIncidentsCount: retrieveSingleStoreIncidentsCountReducer,
         singleStoreIncidentsValue: retrieveSingleStoreIncidentsValueReducer,
-        singleStoreIncidentsAverage: retrieveSingleStoreAverageIncidentValueReducer,
         topTwentyMostValuableIncidentsInAStore: retrieveTopTwentyMostValuableIncidentsInAStoreReducer,
         theMostNotoriousStoreSection: retrieveTheMostNotoriousStoreSectionReducer,
         dateState: dateReducer,
         theTopTwentyIncidentsInARegion: retrieveTheTopTwentyIncidentsInARegionReducer,
         singleRegionIncidentsCount: retrieveTheNumberOfIncidentsInAGivenRegionReducer,
         singleRegionIncidentsValue: retrieveTheTotalValueOfAllIncidentsInARegionReducer,
-        singleRegionIncidentsAverage: retrieveTheAverageValueOfAllIncidentsInARegionReducer,
         theMostNotoriousStoreInARegion: retrieveTheMostNotoriousStoreInARegionReducer,
         topTwentyMostValuableIncidentsOverall: retrieveTheTopTwentyMostNotoriousOverallIncidentsReducer,
         overallIncidentsCount: retrieveTheNumberOfOverallIncidentsReducer,
         overallIncidentsValue: retrieveTheValueOfOverallIncidentsReducer,
-        overallIncidentsAverage: retrieveTheAverageValueOfOverallIncidentsReducer,
         theMostNotoriousRegionOverall: retrieveTheMostNotoriousRegionReducer,
         theTopTwentyIncidentsInAStoreSection: retrieveTheTopTwentyIncidentsInAStoreSectionReducer,
         theValueOfAllIncidentsInAStoreSection: retrieveTheValueOfAllIncidentsInAStoreSectionReducer,
         theNumberOfAllTheIncidentsInAStoreSection: retrieveTheNumberOfAllTheIncidentsInAStoreSectionReducer,
-        theAverageValueOfAllIncidentsInAStoreSection: retrieveTheAverageValueOfAllTheIncidentsInAStoreSectionReducer,
         theMostNotoriousIncidentInAStoreSection: retrieveTheMostNotoriousIncidentInAStoreSectionReducer,
         singleEmployee: retrieveSingleEmployeeReducer,
         numberOfIncidentsReportedByAnEmployee: retrieveTheNumberOfIncidentsReportedByAnEmployeeReducer,
         valueOfIncidentsReportedByAnEmployee: retrieveTheValueOfIncidentsReportedByAnEmployee,
-        avgValueOfIncidentsReportedByAnEmployee: retrieveTheAverageValueOfIncidentsReportedByAnEmployeeReducer,
         theMostNotoriousIncidentReportedByAnEmployee: retrieveTheMostNotoriousIncidentReportedByAnEmployeeReducer,
         theTenMostNotoriousIncidentsReportedByAnEmployee: retrieveTheTenMostNotoriousIncidentReportedByAnEmployeeReducer,
     }

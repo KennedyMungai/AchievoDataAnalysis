ebb22dc234b11b7b323aa3c2ca682276c8d51718
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-08 14:18:33 +0300

    Created the slice file for the store graphing data

---
+++ frontend/redux/features/stores/retrieveTheStoreGraphingDataSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit"
+import axios from "axios"
+import { RootState } from "../store"
+
+
+export const retrieveTheStoreGraphingData = createAsyncThunk('stores/graphingData', async (storeId: number) => {
+    const response = await axios.get(`http://localhost:8000/incidents/store/value/store_sections/${storeId}`)
+    return response.data
+})
+
+
+const initialState: IGraphData = {
+    is_loaded: false,
+    labels: [],
+    data: []
+}
+
+
+export const retrieveTheStoreGraphingDataSlice = createSlice({
+    name: 'graphingData',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(retrieveTheStoreGraphingData.pending, (state) => {
+                state.is_loaded = false
+            })
+            .addCase(retrieveTheStoreGraphingData.fulfilled, (state, action: PayloadAction<IGraph>) => {
+                state.is_loaded = true
+                state.labels = action.payload.labels
+                state.data = action.payload.data
+            })
+            .addCase(retrieveTheStoreGraphingData.rejected, (state, action) => {
+                state.is_loaded = false
+            })
+    }
+})
+
+export const selectTheGraphingDataLabels = (state: RootState) => state.theStoreGraphingData.labels
+export const selectTheGraphingData = (state: RootState) => state.theStoreGraphingData.data
+
+export default retrieveTheStoreGraphingDataSlice.reducer

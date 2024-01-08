c8faa826f47e03bacb5d1bec3796079771ca7719
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-08 15:36:30 +0300

    Implemented the charting data to the region value charts

--- frontend/components/Charts/RegionValueChart.tsx
+++ frontend/components/Charts/RegionValueChart.tsx
@@ -2,6 +2,11 @@
 import Chart from 'chart.js/auto'
 import { CategoryScale } from 'chart.js'
 import { Line } from 'react-chartjs-2'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import {
+	selectARegionsGraphingData,
+	selectARegionsGraphingDataLabels
+} from '@/redux/features/regions/retrieveARegionsGraphingDataSlice'
 
 type Props = {
 	regionId: number
@@ -10,12 +15,16 @@ type Props = {
 Chart.register(CategoryScale)
 
 const RegionValueChart = ({ regionId: storeId }: Props) => {
-	const data = {
-		labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+	const dispatch = useAppDispatch()
+	const data = useAppSelector(selectARegionsGraphingData)
+	const labels = useAppSelector(selectARegionsGraphingDataLabels)
+
+	const graphingData = {
+		labels,
 		datasets: [
 			{
 				label: 'Stores $',
-				data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
+				data,
 				borderColor: 'rgb(53, 162, 235)',
 				backgroundColor: 'rgb(53, 162, 235, 0.4'
 			}
@@ -37,7 +46,7 @@ const RegionValueChart = ({ regionId: storeId }: Props) => {
 
 	return (
 		<div className='lg:w-full w-[80vw] border rounded-lg bg-white h-[60vh] dark:bg-slate-900 p-5 flex items-center justify-center'>
-			<Line options={options} data={data} />
+			<Line options={options} data={graphingData} />
 		</div>
 	)
 }

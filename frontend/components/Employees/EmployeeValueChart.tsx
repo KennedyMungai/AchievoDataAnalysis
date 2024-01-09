88d9f178a7adf2328fc99a820a4925211e1291ec
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-09 09:34:13 +0300

    Added the dynamic table data to the EmployyeValueChart

--- frontend/components/Employees/EmployeeValueChart.tsx
+++ frontend/components/Employees/EmployeeValueChart.tsx
@@ -2,6 +2,9 @@
 import Chart from 'chart.js/auto'
 import { CategoryScale } from 'chart.js'
 import { Line } from 'react-chartjs-2'
+import { useAppDispatch, useAppSelector } from '@/redux/hooks'
+import { retrieveTheEmployeeGraphingData, selectTheEmployeeGraphingData, selectTheEmployeeGraphingDataLabels } from '@/redux/features/employees/retrieveTheEmployeeGraphingDataSlice'
+import { useEffect } from 'react'
 
 type Props = {
     employeeId: number
@@ -10,12 +13,21 @@ type Props = {
 Chart.register(CategoryScale)
 
 const EmployeeValueChart = ({employeeId}: Props) => {
-    const data = {
-		labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+	const dispatch = useAppDispatch()
+	const data = useAppSelector(selectTheEmployeeGraphingData)
+	const labels = useAppSelector(selectTheEmployeeGraphingDataLabels)
+
+	useEffect(() => {
+		dispatch(retrieveTheEmployeeGraphingData(employeeId))
+	}, [])
+	
+
+    const graphingData = {
+		labels,
 		datasets: [
 			{
 				label: 'Incidents $',
-				data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
+				data,
 				borderColor: 'rgb(53, 162, 235)',
 				backgroundColor: 'rgb(53, 162, 235, 0.4'
 			}
@@ -37,7 +49,7 @@ const EmployeeValueChart = ({employeeId}: Props) => {
 
 	return (
 		<div className='lg:w-full w-[80vw] border rounded-lg bg-white h-[60vh] dark:bg-slate-900 p-5 flex items-center justify-center'>
-			<Line options={options} data={data} />
+			<Line options={options} data={graphingData} />
 		</div>
 	)
 }

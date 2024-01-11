b54cd02633992bfd23691747cb48f9250fe77d88
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-11 12:32:05 +0300

    Added some conditional rendering for the employees button

--- frontend/components/SideBar/SideBar.tsx
+++ frontend/components/SideBar/SideBar.tsx
@@ -1,10 +1,10 @@
 'use client'
+import { selectAuthStateData } from '@/redux/features/auth/authSlice'
+import { useAppSelector } from '@/redux/hooks'
 import Image from 'next/image'
 import Link from 'next/link'
 import { BsPerson } from 'react-icons/bs'
-import { IoStorefrontOutline } from 'react-icons/io5'
 import { SlMap } from 'react-icons/sl'
-import { HiOutlineDocumentReport } from 'react-icons/hi'
 import { Separator } from '../ui/separator'
 import {
 	Tooltip,
@@ -17,6 +17,9 @@ import { ThemeToggle } from './ThemeToggle'
 type Props = {}
 
 const SideBar = (props: Props) => {
+	const { is_logged_in, employee_job_title } =
+		useAppSelector(selectAuthStateData)
+
 	return (
 		<nav className='w-[5rem] h-screen flex flex-col justify-between border-r-[1px] dark:border-slate-500 border-slate-300 p-5 fixed bg-slate-50 dark:bg-slate-900 scrollbar-hide'>
 			<div className=''>
@@ -47,18 +50,23 @@ const SideBar = (props: Props) => {
 						</TooltipProvider>
 					</div>
 				</Link>
-				<Link href={'/employees'}>
-					<div className='text-3xl p-2 text-slate-700 dark:text-slate-200 hover:bg-slate-300 dark:hover:bg-slate-700 flex items-center justify-center rounded-md'>
-						<TooltipProvider>
-							<Tooltip>
-								<TooltipTrigger>
-									<BsPerson />
-								</TooltipTrigger>
-								<TooltipContent>Employees</TooltipContent>
-							</Tooltip>
-						</TooltipProvider>
-					</div>
-				</Link>
+				{is_logged_in &&
+					employee_job_title === 'EmployeeJobTitle.ADMINISTRATOR' && (
+						<Link href={'/employees'}>
+							<div className='text-3xl p-2 text-slate-700 dark:text-slate-200 hover:bg-slate-300 dark:hover:bg-slate-700 flex items-center justify-center rounded-md'>
+								<TooltipProvider>
+									<Tooltip>
+										<TooltipTrigger>
+											<BsPerson />
+										</TooltipTrigger>
+										<TooltipContent>
+											Employees
+										</TooltipContent>
+									</Tooltip>
+								</TooltipProvider>
+							</div>
+						</Link>
+					)}
 			</div>
 			<div className=''>
 				<ThemeToggle />

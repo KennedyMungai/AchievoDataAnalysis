eedc7c9e8da6fd2bce9e309a8e2418a35d1f3ec9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-11 12:47:05 +0300

    Made sure only the administrator can see the context menu

--- frontend/components/Stores/StoreCardTemplate.tsx
+++ frontend/components/Stores/StoreCardTemplate.tsx
@@ -1,23 +1,25 @@
 'use client'
+import { selectAuthStateData } from '@/redux/features/auth/authSlice'
+import { useAppSelector } from '@/redux/hooks'
 import axios from 'axios'
 import moment from 'moment'
 import Link from 'next/link'
+import { ReactNode } from 'react'
 import toast from 'react-hot-toast'
 import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle
 } from '../ui/card'
 import {
-    ContextMenu,
-    ContextMenuContent,
-    ContextMenuItem,
-    ContextMenuTrigger
+	ContextMenu,
+	ContextMenuContent,
+	ContextMenuItem,
+	ContextMenuTrigger
 } from '../ui/context-menu'
 import { Separator } from '../ui/separator'
-import { ReactNode } from 'react'
 
 type Props = {
 	title: string
@@ -34,48 +36,73 @@ const StoreCardTemplate = ({
 	storeId,
 	title
 }: Props) => {
-	return (
-		<ContextMenu>
-			<ContextMenuTrigger>
-				<Link href={link}>
-					<Card className='w-[20rem] h-[25rem] dark:hover:bg-slate-900/50 hover:bg-slate-100/50 hover:shadow-md'>
-						<CardHeader>
-							<CardTitle className='text-center'>
-								{title}
-							</CardTitle>
-						</CardHeader>
-						<Separator />
-						<CardContent className='flex flex-1 py-20 items-center justify-center'>
-							{content}
-						</CardContent>
-						<Separator />
-						<CardFooter className='flex items-end justify-end w-full'>
-							<p className='text-sm '>
-								{moment(createdAt).format(
-									'DD-MMM-YYYY'
-								)}
-							</p>
-						</CardFooter>
-					</Card>
-				</Link>
-			</ContextMenuTrigger>
-			<ContextMenuContent>
-				<ContextMenuItem onClick={() => console.log(storeId)}>
-					Edit
-				</ContextMenuItem>
-				<ContextMenuItem
-					onClick={() => {
-						axios.delete(`http://localhost:8000/stores/${storeId}`)
-						toast.success(
-							'Store Successfully Deleted (Reload page to see changes)'
-						)
-					}}
-				>
-					Delete
-				</ContextMenuItem>
-			</ContextMenuContent>
-		</ContextMenu>
-	)
+	const { is_logged_in, employee_job_title } =
+		useAppSelector(selectAuthStateData)
+
+	const renderedContent =
+		is_logged_in &&
+		employee_job_title === 'EmployeeJobTitle.ADMINISTRATOR' ? (
+			<ContextMenu>
+				<ContextMenuTrigger>
+					<Link href={link}>
+						<Card className='w-[20rem] h-[25rem] dark:hover:bg-slate-900/50 hover:bg-slate-100/50 hover:shadow-md'>
+							<CardHeader>
+								<CardTitle className='text-center'>
+									{title}
+								</CardTitle>
+							</CardHeader>
+							<Separator />
+							<CardContent className='flex flex-1 py-20 items-center justify-center'>
+								{content}
+							</CardContent>
+							<Separator />
+							<CardFooter className='flex items-end justify-end w-full'>
+								<p className='text-sm '>
+									{moment(createdAt).format('DD-MMM-YYYY')}
+								</p>
+							</CardFooter>
+						</Card>
+					</Link>
+				</ContextMenuTrigger>
+				<ContextMenuContent>
+					<ContextMenuItem onClick={() => console.log(storeId)}>
+						Edit
+					</ContextMenuItem>
+					<ContextMenuItem
+						onClick={() => {
+							axios.delete(
+								`http://localhost:8000/stores/${storeId}`
+							)
+							toast.success(
+								'Store Successfully Deleted (Reload page to see changes)'
+							)
+						}}
+					>
+						Delete
+					</ContextMenuItem>
+				</ContextMenuContent>
+			</ContextMenu>
+		) : (
+			<Link href={link}>
+				<Card className='w-[20rem] h-[25rem] dark:hover:bg-slate-900/50 hover:bg-slate-100/50 hover:shadow-md'>
+					<CardHeader>
+						<CardTitle className='text-center'>{title}</CardTitle>
+					</CardHeader>
+					<Separator />
+					<CardContent className='flex flex-1 py-20 items-center justify-center'>
+						{content}
+					</CardContent>
+					<Separator />
+					<CardFooter className='flex items-end justify-end w-full'>
+						<p className='text-sm '>
+							{moment(createdAt).format('DD-MMM-YYYY')}
+						</p>
+					</CardFooter>
+				</Card>
+			</Link>
+		)
+
+	return renderedContent
 }
 
 export default StoreCardTemplate

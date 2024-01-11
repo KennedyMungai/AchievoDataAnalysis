6dea11c44927b7237abb9d6e7f52efe241737ba2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-11 10:30:07 +0300

    Added some of the logic used to authenticate users

--- frontend/redux/features/auth/authSlice.ts
+++ frontend/redux/features/auth/authSlice.ts
@@ -1,5 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import axios from 'axios'
 import { RootState } from "../store"
+
+
+export const getAuthToken = createAsyncThunk("auth/login", async (values: ICredentials) => {
+    const credentialsForm = new FormData()
+    credentialsForm.append("username", values.employee_email)
+    credentialsForm.append("password", values.employee_password)
+
+    const response = await axios.post("http://localhost:8000/auth/login", credentialsForm)
+    const data = response.data
+
+    return data
+})
+
 
 const initialState: IToken = {
     token_type: "",
@@ -24,6 +38,23 @@ const authSlice = createSlice({
             state.access_token = ""
             state.employee_id = 0
         }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(getAuthToken.fulfilled, (state, action) => {
+                state.is_loaded = true
+                state.token_type = action.payload.token_type
+                state.access_token = action.payload.access_token
+                state.employee_id = action.payload.employee_id
+                state.employee_job_title = action.payload.employee_job_title
+                state.employee_name = action.payload.employee_name
+            })
+            .addCase(getAuthToken.rejected, (state) => {
+                state.is_loaded = false
+            })
+            .addCase(getAuthToken.pending, (state) => {
+                state.is_loaded = false
+            })
     }
 })
 

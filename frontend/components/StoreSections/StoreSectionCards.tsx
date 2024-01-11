386f1a8caf7478f62c699cd7bcc127de5e37eb55
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-11 13:04:43 +0300

    Made the add store section card only visible to administrators

--- frontend/components/StoreSections/StoreSectionCards.tsx
+++ frontend/components/StoreSections/StoreSectionCards.tsx
@@ -1,13 +1,14 @@
 'use client'
 import {
-    retrieveAllOfAStoresStoreSections,
-    selectAllStoreSections
+	retrieveAllOfAStoresStoreSections,
+	selectAllStoreSections
 } from '@/redux/features/storeSections/retrieveAllStoreStoreSectionsSlice'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import { useEffect } from 'react'
 import AddStoreSectionCard from './AddStoreSectionCard'
 import StoreSectionCard from './StoreSectionCard'
-import { LuClipboardList } from "react-icons/lu"
+import { LuClipboardList } from 'react-icons/lu'
+import { selectAuthStateData } from '@/redux/features/auth/authSlice'
 
 type Props = {
 	storeId: number
@@ -15,6 +16,9 @@ type Props = {
 
 const StoreSectionCards = ({ storeId }: Props) => {
 	const storeSections = useAppSelector(selectAllStoreSections)
+	const { is_logged_in, employee_job_title } =
+		useAppSelector(selectAuthStateData)
+
 	const dispatch = useAppDispatch()
 
 	useEffect(() => {
@@ -25,15 +29,18 @@ const StoreSectionCards = ({ storeId }: Props) => {
 		<div className='w-full flex flex-wrap justify-center gap-2 p-4'>
 			{storeSections.map((storeSection) => (
 				<StoreSectionCard
-                    key={storeSection.store_section_id}
+					key={storeSection.store_section_id}
 					title={storeSection.store_section_name}
 					link={`/storeSections/singleStoreSection/${storeSection.store_section_id}`}
-					content={<LuClipboardList className="text-8xl" />}
+					content={<LuClipboardList className='text-8xl' />}
 					createdAt={storeSection.created_at}
 					storeSectionId={0}
 				/>
 			))}
-            <AddStoreSectionCard store_id={storeId} />
+			{is_logged_in &&
+				employee_job_title === 'EmployeeJobTitle.ADMINISTRATOR' && (
+					<AddStoreSectionCard store_id={storeId} />
+				)}
 		</div>
 	)
 }

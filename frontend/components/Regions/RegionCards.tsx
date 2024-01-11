ce07c905520e0f9dedbbbfad030c11a9326034a7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-11 12:42:43 +0300

    Made the add region card only viewable by an administrator

--- frontend/components/Regions/RegionCards.tsx
+++ frontend/components/Regions/RegionCards.tsx
@@ -3,18 +3,25 @@ import React, { useEffect } from 'react'
 import RegionCardTemplate from '../Templates/RegionCardTemplate'
 import AddRegionCardTemplate from '../Templates/AddRegionCardTemplate'
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
-import { retrieveAllRegions, selectAllRegions } from '@/redux/features/regions/retrieveAllRegionsSlice'
-import { LuMapPin } from "react-icons/lu"
+import {
+	retrieveAllRegions,
+	selectAllRegions
+} from '@/redux/features/regions/retrieveAllRegionsSlice'
+import { LuMapPin } from 'react-icons/lu'
+import { selectAuthStateData } from '@/redux/features/auth/authSlice'
 
 type Props = {}
 
 const RegionCards = (props: Props) => {
 	const regions = useAppSelector(selectAllRegions)
+	const { is_logged_in, employee_job_title } =
+		useAppSelector(selectAuthStateData)
+
 	const dispatch = useAppDispatch()
 
 	useEffect(() => {
-        dispatch(retrieveAllRegions())
-    }, [])
+		dispatch(retrieveAllRegions())
+	}, [])
 
 	return (
 		<div className='p-5 w-90 flex flex-wrap items-center justify-center gap-2'>
@@ -24,13 +31,16 @@ const RegionCards = (props: Props) => {
 						key={region.region_id}
 						title={region.region_name}
 						link={`regions/${region.region_id}`}
-						content= {<LuMapPin className='text-8xl' />}
+						content={<LuMapPin className='text-8xl' />}
 						createdAt={region.created_at!}
 						regionId={region.region_id}
 					/>
 				)
 			})}
-			<AddRegionCardTemplate />
+			{is_logged_in &&
+				employee_job_title === 'EmployeeJobTitle.ADMINISTRATOR' && (
+					<AddRegionCardTemplate />
+				)}
 		</div>
 	)
 }

a2ebc20c0046a03621da19f8f32cfab7b1af187c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-11 09:32:54 +0300

    Got rid of unused imports from the authSlice

--- frontend/redux/features/auth/authSlice.ts
+++ frontend/redux/features/auth/authSlice.ts
@@ -1,5 +1,4 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import axios from 'axios'
+import { createSlice } from "@reduxjs/toolkit"
 import { RootState } from "../store"
 
 const initialState: IToken = {
